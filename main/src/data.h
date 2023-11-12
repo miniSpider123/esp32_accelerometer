9aaad9492e56eb7f76a3486f737359bcac99dda3
miniSpider123 <ankapajak4@gmail.com>
2023-11-12 01:00:59 +0100

    asdf

--- main/src/data.h
+++ main/src/data.h
@@ -1,1 +1,8 @@
-...
+#define FIREBASE_HOST "https://esp-mgr-default-rtdb.europe-west1.firebasedatabase.app/"
+#define API_KEY "AIzaSyA8f2RjQS7JENP9kYXL0X6aZmjvh1XMlsk"
+#define USER_EMAIL "ankapajak4@gmail.com"
+#define USER_PASSWORD "ESP123mgr"
+#define ESP_WIFI_SSID "wunsz_rzeczny"
+#define ESP_WIFI_PASS "9eed0d14371c"
+#define ESP_MAXIMUM_RETRY 10
+#define DELAY_TIME 10000
